a6457e5ef1856fcfcfa0e92c637e8c319f9c8ade
Zeyu Liao <liao.ze@northeastern.edu>
2024-10-06 02:27:24 -0700

    small updates

---
+++ bitcoin-explorer-ui/src/components/tbd.tsx
@@ -0,0 +1,12 @@
+// Tbd.tsx
+import React from 'react';
+
+const Tbd: React.FC = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>TBD</h1>
+        </div>
+    );
+};
+
+export default Tbd;
